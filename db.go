e070cb86c036d5020bf794a477d0127025e53dfa
Snyper <snyper@Snypers-MacBook-Pro.local>
2023-05-24 14:58:42 -0400

    feat: dotenv and db connection

--- db.go
+++ db.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"database/sql"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+type RentalsDB struct {
+	*sql.DB
+}
+
+func InitDB() (*RentalsDB, error) {
+
+	dbname := os.Getenv("dbname")
+	user := os.Getenv("user")
+	password := os.Getenv("password")
+	host := os.Getenv("host")
+	sslmode := os.Getenv("sslmode")
+
+	dsn := "dbname=" + dbname +
+		"\nuser=" + user +
+		"\npassword=" + password +
+		"\nhost=" + host +
+		"\nsslmode=" + sslmode
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return &RentalsDB{db}, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Initialize database
+	db, err := InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
